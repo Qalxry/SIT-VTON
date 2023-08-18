fea0b5ebd8ad49a84c85c4bc4e1fb9d46ca0e1eb
Qalxry <qalxry@qq.com>
2023-08-18 19:04:44 +0800

    Update the download urls of pose_iter_584000.caffemodel.pt and CIHP_pgn.zip

--- download.py
+++ download.py
@@ -31,7 +31,8 @@ file_names = [
 ]
 
 file_urls = [
-    'https://drive.google.com/uc?id=1ij6KxxeBkOjox-kc6RFCQMU_3QY2krR3&export=download&confirm=t&uuid=96966ebb-4c84-4fa0-baa0-38ad425bd773&at=AB6BwCDjSoOUHd2Xo6XIcaPZpxPA:1692348756164',
+    # 'https://drive.google.com/uc?id=1ij6KxxeBkOjox-kc6RFCQMU_3QY2krR3&export=download&confirm=t&uuid=96966ebb-4c84-4fa0-baa0-38ad425bd773&at=AB6BwCDjSoOUHd2Xo6XIcaPZpxPA:1692348756164',
+    'https://github.com/Qalxry/SIT-VTON/releases/download/models/pose_iter_584000.caffemodel.pt',
     'https://github.com/miccunifi/ladi-vton/releases/download/weights/emasc_vitonhd.pth',
     'https://github.com/miccunifi/ladi-vton/releases/download/weights/inversion_adapter_vitonhd.pth',
     'https://github.com/miccunifi/ladi-vton/releases/download/weights/unet_vitonhd.pth',
@@ -39,7 +40,7 @@ file_urls = [
     'https://huggingface.co/stabilityai/stable-diffusion-2-inpainting/blob/main/text_encoder/pytorch_model.bin',
     'https://huggingface.co/stabilityai/stable-diffusion-2-inpainting/blob/main/vae/diffusion_pytorch_model.bin',
     'https://huggingface.co/laion/CLIP-ViT-H-14-laion2B-s32B-b79K/blob/main/pytorch_model.bin',
-    'https://github.com/Qalxry/SIT-VTON/releases/download/model/CIHP_pgn.zip'
+    'https://github.com/Qalxry/SIT-VTON/releases/download/models/CIHP_pgn.zip'
 ]
 
 file_urls_backup = []
